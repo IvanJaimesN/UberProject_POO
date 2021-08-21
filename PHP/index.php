a18ccd4c9ef0aaf657799c579de39ab2d716a837
Iván Jaimes <ivan2080508@gmail.com>
2021-08-21 16:30:44 -0500

    index update

--- PHP/index.php
+++ PHP/index.php
@@ -1,8 +1,18 @@
 <?php
 
 require_once('car.php');
+require_once('uberX.php');
+require_once('uberPool.php');
 require_once('account.php');
 
 $car = new Car("AW563", new Account("Andres Herrera", "AND98559"));
 
 $car->PrintDataCar();
+
+$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND8573"), "Chevrolet", "Spark");
+$uberX->PrintDataCar();
+
+$uberPool = new uberPool("XPS665", new Account("Andrea Paez", "ANDRE123"), "Dodge", "Attitude");
+$uberPool->PrintDataCar();
+
+?>
